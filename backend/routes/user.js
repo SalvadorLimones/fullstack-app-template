491a4104f85cba8dd4ac137ab4a9d6d1e8ff3b03
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-06-09 12:44:16 +0200

    add multiple account login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const httpModule = require("../utils/http");
 const http = httpModule();
+const { auth } = require("../middlewares/auth");
 
 const config = {
   google: {
@@ -30,7 +31,7 @@ const config = {
   }, */
 };
 
-router.post("/login", async (req, res) => {
+router.post("/login", auth({ block: false }), async (req, res) => {
   const payload = req.body;
   if (!payload) return res.status(400).send("All inputs are required 1");
 
@@ -55,13 +56,13 @@ router.post("/login", async (req, res) => {
       },
     }
   );
-  console.log("RESP: ", response);
 
   if (!response) return res.sendStatus(500);
   if (response.status !== 200) return res.sendStatus(401);
 
   let openId;
   const onlyOauth = !response.data.id_token;
+
   if (onlyOauth) {
     //let token = response.data.split("=")[1].split("&")[0];
     let token = response.data.access_token;
@@ -75,8 +76,8 @@ router.post("/login", async (req, res) => {
       }
     );
 
-    if (!response) return res.sendStatus(500);
-    if (response.status !== 200) return res.sendStatus(401);
+    if (!userResponse) return res.sendStatus(500);
+    if (userResponse.status !== 200) return res.sendStatus(401);
 
     const id = config[provider].user_id;
     openId = userResponse.data[id];
@@ -86,15 +87,55 @@ router.post("/login", async (req, res) => {
     openId = decoded.sub;
   }
 
-  //megkeresi a user-t, ha nincs csinál egyet:
-  const key = "providers." + provider;
-  const user = await User.findOneAndUpdate(
+  /*   const user = await User.findOneAndUpdate(
     { [key]: openId },
     { providers: { [provider]: openId } },
     { new: true, upsert: true }
-  );
+  ); */
+
+  //példa optional chainingre , b1 és c1 értéke ugyanaz (ha van user.profile.account.balance,akkor nannak az értéke, ha nem, akkor null)
+  /*   const user = {
+    username: "Random",
+    profile: { id: "123456", account: { balance: 0, account_id: "bb" } },
+  };
+  const b1 =
+    user && user.profile && user.profile.account && user.profile.account.balance
+      ? user.profile.account.balance
+      : null;
+      
+      const c1 = user?.profile?.account?.balance; */
+
+  //megkeresi a user-t, ha nincs csinál egyet:
+  const key = "providers." + provider;
+  let user = await User.findOne({ [key]: openId });
+  if (user && res.locals.user?.providers) {
+    user.providers = { ...user.providers, ...res.locals.user.providers };
+    user = await user.save();
+  }
+
   const sessionToken = jwt.sign(
-    { userId: user._id, providers: user.providers },
+    {
+      userId: user?._id, //optional chaning
+      providers: user ? user.providers : { [provider]: openId }, //OC itt nem jó, mert nem null-t adunk vissza
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+  res.json({ sessionToken });
+});
+
+router.post("/create", auth({ block: true }), async (req, res) => {
+  if (!req.body?.username) return res.sendStatus(400);
+  const user = await User.create({
+    username: req.body.username,
+    providers: res.locals.user.providers,
+  });
+
+  const sessionToken = jwt.sign(
+    {
+      userId: user._id,
+      providers: user.providers,
+    },
     process.env.JWT_SECRET,
     { expiresIn: "1h" }
   );
