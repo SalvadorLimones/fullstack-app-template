0b8e4b7a3819d0f303a8ff2f4cf9558f6aae3caa
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-06-10 10:52:13 +0200

    add app.config

--- backend/models/user.js
+++ backend/models/user.js
@@ -15,9 +15,9 @@ const userSchema = new mongoose.Schema({
   username: { type: String }, //empty string NONO!  !!unique
   email: { type: String }, //empty string NONO! + validation
   providers: {
-    google: { type: String },
-    facebook: { type: String },
-    github: { type: String },
+    google: { type: String, unique: true, sparse: true },
+    facebook: { type: String, unique: true, sparse: true },
+    github: { type: String, unique: true, sparse: true },
   },
   dashboards: [dashboardSchema], //empty list as default?
 });
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,35 +1,10 @@
-require("dotenv").config();
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const httpModule = require("../utils/http");
 const http = httpModule();
 const { auth } = require("../middlewares/auth");
-
-const config = {
-  google: {
-    client_id: process.env.GOOGLE_CLIENT_ID,
-    client_secret: process.env.GOOGLE_CLIENT_SECRET,
-    redirect_uri: process.env.GOOGLE_REDIRECT_URI,
-    token_endpoint: process.env.GOOGLE_TOKEN_ENDPOINT,
-    user_endpoint: process.env.GOOGLE_USER_ENDPOINT,
-    user_id: process.env.GOOGLE_USER_ID,
-  },
-  github: {
-    client_id: process.env.GIT_CLIENT_ID,
-    client_secret: process.env.GIT_CLIENT_SECRET,
-    redirect_uri: process.env.GIT_REDIRECT_URI,
-    token_endpoint: process.env.GIT_TOKEN_ENDPOINT,
-    user_endpoint: process.env.GIT_USER_ENDPOINT,
-    user_id: process.env.GIT_USER_ID,
-  },
-  /*   facebook: {
-    clientId: "",
-    clientSecret: "",
-    redirectUri: "",
-    tokenEndpoint: "",
-  }, */
-};
+const config = require("../app.config");
 
 router.post("/login", auth({ block: false }), async (req, res) => {
   const payload = req.body;
@@ -38,16 +13,16 @@ router.post("/login", auth({ block: false }), async (req, res) => {
   const code = payload.code;
   const provider = payload.provider;
   if (!(code && provider)) return res.status(400).send("All inputs required 2");
-  if (!Object.keys(config).includes(provider))
+  if (!Object.keys(config.auth).includes(provider))
     return res.status(400).send("Wrong payload!");
 
   const response = await http.post(
-    config[provider].token_endpoint,
+    config.auth[provider].token_endpoint,
     {
       code: code,
-      client_id: config[provider].client_id,
-      client_secret: config[provider].client_secret,
-      redirect_uri: config[provider].redirect_uri,
+      client_id: config.auth[provider].client_id,
+      client_secret: config.auth[provider].client_secret,
+      redirect_uri: config.auth[provider].redirect_uri,
       grant_type: "authorization_code",
     },
     {
@@ -67,7 +42,7 @@ router.post("/login", auth({ block: false }), async (req, res) => {
     //let token = response.data.split("=")[1].split("&")[0];
     let token = response.data.access_token;
     const userResponse = await http.post(
-      config[provider].user_endpoint,
+      config.auth[provider].user_endpoint,
       {},
       {
         headers: {
@@ -79,7 +54,7 @@ router.post("/login", auth({ block: false }), async (req, res) => {
     if (!userResponse) return res.sendStatus(500);
     if (userResponse.status !== 200) return res.sendStatus(401);
 
-    const id = config[provider].user_id;
+    const id = config.auth[provider].user_id;
     openId = userResponse.data[id];
   } else {
     const decoded = jwt.decode(response.data.id_token);
@@ -93,7 +68,7 @@ router.post("/login", auth({ block: false }), async (req, res) => {
     { new: true, upsert: true }
   ); */
 
-  //példa optional chainingre , b1 és c1 értéke ugyanaz (ha van user.profile.account.balance,akkor nannak az értéke, ha nem, akkor null)
+  //példa optional chainingre , b1 és c1 értéke ugyanaz (ha van user.profile.account.balance,akkor annak az értéke, ha nem, akkor null)
   /*   const user = {
     username: "Random",
     profile: { id: "123456", account: { balance: 0, account_id: "bb" } },
