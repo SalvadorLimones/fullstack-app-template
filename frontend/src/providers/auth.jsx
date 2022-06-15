c6a5054fa793cabadb04878593fad7b9494ef8f4
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-06-15 10:08:18 +0200

    ff

--- frontend/src/providers/auth.jsx
+++ frontend/src/providers/auth.jsx
@@ -4,7 +4,21 @@ const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
-  const auth = () => {};
+  const auth = () => {
+    const googleBaseUrl = "https://accounts.google.com/o/oauth2/v2/auth";
+    const searchParams = new URLSearchParams();
+    searchParams.append(
+      "client_id",
+      "423125049963-vnhlm59vvirdjsquu0efhqvq5u91orks.apps.googleusercontent.com"
+    );
+    searchParams.append("scope", "openid");
+    searchParams.append("redirect_uri", "http://localhost:3000/callback");
+    searchParams.append("response_type", "code");
+    searchParams.append("prompt", "select_account");
+
+    const fullUrl = googleBaseUrl + "?" + searchParams.toString();
+    window.open(fullUrl);
+  };
   const logout = () => setToken(null);
   const contextValue = { token, auth, logout };
 
@@ -17,6 +31,6 @@ const AuthProvider = ({ children }) => {
   );
 };
 
-const useCounter = () => useContext(AuthContext);
+const useAuth = () => useContext(AuthContext);
 
-export { AuthProvider, useCounter };
+export { AuthProvider, useAuth };
