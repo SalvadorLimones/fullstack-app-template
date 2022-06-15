f2230d2e0d8050b4ff040611de14b6617be6e6c0
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-06-15 10:21:31 +0200

    profile page fix

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-
+import { useCounter } from "../hooks/useCounter";
 import { useCounter as useGlobalCounter } from "../providers/counter";
 
 const Profile = () => {
-  const { value, setValue, valueProfile, setValueProfile } = useGlobalCounter();
-
+  const { counter, increment, decrement } = useCounter();
+  const { value, increment: goUp, decrement: goDown } = useGlobalCounter();
   return (
     <>
       <div>Profile</div>
       <h3>Local counter:</h3>
-      <p>{valueProfile}</p>
-      <button onClick={() => setValueProfile(valueProfile - 1)}>-</button>
-      <button onClick={() => setValueProfile(valueProfile + 1)}>+</button>
+      <p>{counter}</p>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
       <h3>Global counter:</h3>
       <p>{value}</p>
-      <button onClick={() => setValue(value - 1)}>-</button>
-      <button onClick={() => setValue(value + 1)}>+</button>
+      <button onClick={goDown}>-</button>
+      <button onClick={goUp}>+</button>
     </>
   );
 };
