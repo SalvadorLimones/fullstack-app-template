74c7b6aa091dee5312df4394e47dabba87075018
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-06-15 13:41:30 +0200

    add valueparams to counterProvider

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,25 +1,29 @@
 import React from "react";
-import { useCounter } from "../hooks/useCounter";
+//import { useCounter } from "../hooks/useCounter";
 import { useCounter as useGlobalCounter } from "../providers/counter";
 import { useAuth } from "../providers/auth";
 
 const Profile = () => {
-  const { counter, increment, decrement } = useCounter();
-  const { value, increment: goUp, decrement: goDown } = useGlobalCounter();
+  // const { counter, increment, decrement } = useCounter();
+  const {
+    value,
+    valueProfile,
+    increment: goUp,
+    decrement: goDown,
+  } = useGlobalCounter();
   const { token } = useAuth();
-  console.log(token);
   return (
     <>
       <div>Profile</div>
       <p>{token ? "Logged in" : "Anonymus user"}</p>
       <h3>Local counter:</h3>
-      <p>{counter}</p>
-      <button onClick={decrement}>-</button>
-      <button onClick={increment}>+</button>
+      <p>{valueProfile}</p>
+      <button onClick={() => goDown("profile")}>-</button>
+      <button onClick={() => goUp("profile")}>+</button>
       <h3>Global counter:</h3>
       <p>{value}</p>
-      <button onClick={goDown}>-</button>
-      <button onClick={goUp}>+</button>
+      <button onClick={() => goDown()}>-</button>
+      <button onClick={() => goUp()}>+</button>
     </>
   );
 };
--- frontend/src/providers/counter.jsx
+++ frontend/src/providers/counter.jsx
@@ -4,9 +4,20 @@ const CounterContext = createContext();
 
 const CounterProvider = ({ children }) => {
   const [value, setValue] = useState(0);
-  const increment = () => setValue(value + 1);
-  const decrement = () => setValue(value - 1);
-  const contextValue = { value, increment, decrement };
+  const [valueHome, setValueHome] = useState(0);
+  const [valueProfile, setValueProfile] = useState(0);
+
+  const increment = (param) => {
+    if (!param) setValue(value + 1);
+    if (param === "home") setValueHome(valueHome + 1);
+    if (param === "profile") setValueProfile(valueProfile + 1);
+  };
+  const decrement = (param) => {
+    if (!param) setValue(value - 1);
+    if (param === "home") setValueHome(valueHome - 1);
+    if (param === "profile") setValueProfile(valueProfile - 1);
+  };
+  const contextValue = { value, valueHome, valueProfile, increment, decrement };
 
   return (
     <div>
